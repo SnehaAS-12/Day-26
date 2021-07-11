abea62ae036fee7e7f4e162e1f02d6a64b7071b9
SnehaAS-12 <85794682+SnehaAS-12@users.noreply.github.com>
2021-07-11 21:39:22 +0530

    Add files via upload

---
+++ day26.py
@@ -0,0 +1,9 @@
+#Enumerate a python list and try to print the counter with the list value
+
+chocolates=["cadbury" ,"5 star","kitkat"]
+print("List :",list(enumerate(chocolates)))
+
+
+#Enumerate a python tuple and try to print the counter with the tuple value
+chocolates=("cadbury" ,"5 star","kitkat")
+print("\nTuple :",tuple(enumerate(chocolates)))
